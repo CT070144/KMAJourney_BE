4b79a539e0c9376f431974f20ca3450a6fd9b46c
NguyenVanPhuc <vanphuctruggc@gmail.com>
2025-05-23 23:02:52 +0700

    @PreAuthorize và @Postauthorize sercuritycontextholder

---
+++ src/main/java/com/example/demo/config/ApplicationInitConfig.java
@@ -0,0 +1,43 @@
+package com.example.demo.config;
+
+import com.example.demo.entity.User;
+import com.example.demo.enums.Role;
+import com.example.demo.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+import java.util.HashSet;
+
+@Slf4j
+@Configuration
+public class ApplicationInitConfig {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Bean
+    ApplicationRunner applicationRunner(UserRepository userRepository){
+
+        return args -> {
+            if(userRepository.findByUserName("admin").isEmpty()){
+                var roles = new HashSet<String>();
+                roles.add(Role.ADMIN.name());
+                User user = User.builder()
+                        .userName("admin")
+                        .passWord(passwordEncoder.encode("123123"))
+                        .roles(roles)
+                        .build();
+                userRepository.save(user);
+
+                log.warn("admin has been created");
+
+
+
+            }
+        };
+    }
+}
